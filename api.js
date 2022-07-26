dd9ff0fb337c32d612b44d03e42994636f927a8c
Yogesh Bansal <yogesh.bansal@live.com>
2022-07-26 22:21:49 +0530

    Added Node parameter to API

--- api.js
+++ api.js
@@ -1,19 +1,21 @@
-const sdkreader = require('@gitcoinco/passport-sdk-reader');
-const reader = new sdkreader.PassportReader('https://ceramic-clay.3boxlabs.com', '1');
-const sdkverifier = require('@gitcoinco/passport-sdk-verifier');
-const verifier = new sdkverifier.PassportVerifier();
 var express = require("express");
 var app = express();
 
-async function verify(add) {    
+async function verify(add,node) {    
+    const sdkreader = require('@gitcoinco/passport-sdk-reader');
+    const reader = new sdkreader.PassportReader(node, '1');
+    const sdkverifier = require('@gitcoinco/passport-sdk-verifier');
+    const verifier = new sdkverifier.PassportVerifier();
     const passport = await reader.getPassport(add);
     const verified = await verifier.verifyPassport(add, passport);
     return(verified)
 }
 
 app.get("/verify", async (req, res) => {
- const result = await verify(req.query.address)
- res.json(result)
+    var address = req.query.address;
+    var node = req.query.node || 'https://ceramic-clay.3boxlabs.com';
+    const result = await verify();
+    res.json(result)
 });
 
 app.listen(3000,'0.0.0.0', () => {
